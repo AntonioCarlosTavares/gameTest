20fd841d839dd9a3deaa3c727c07585deb65b482
AntonioCarlosTavares <actavaresportela@gmail.com>
2017-11-14 13:11:38 -0300

    Add empty project

---
+++ GithubTeste/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+
+	void Update ()
+    {
+        Debug.Log("Olá mundo!");		
+	}
+}
